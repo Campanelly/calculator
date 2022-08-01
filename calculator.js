eaaeea1b15ad94b34b33a64ef38b5ea24e3f2336
Juan Martin Campano <juan.martin.campano@gmail.com>
2022-08-01 19:04:29 -0300

    agrega funcion operate

--- calculator.js
+++ calculator.js
@@ -19,9 +19,31 @@ const multiply = function (firstNumber , secondNumber){
 const divide = function (firstNumber , secondNumber){
     let result;
     if(secondNumber == 0){
-        return "error";
+        return 'error';
     }
     result = firstNumber / secondNumber;
     return result;
 };
+
+const operate = function (operator,firstNumber,secondNumber){
+    
+    if(operator == '+'){
+       return add(firstNumber , secondNumber);
+    }
+    else if(operator == '-'){
+        return substract(firstNumber , secondNumber);
+    }
+    else if(operator == '*'){
+        return multiply(firstNumber , secondNumber);
+    }
+    else if (operator == '/'){
+        return divide(firstNumber , secondNumber);
+    }
+    
+};
+
+console.log(operate('*',545,13));
+console.log(operate('+',200,-30));
+console.log(operate('/',453,83));
+console.log(operate('-',1371,2500));
 
